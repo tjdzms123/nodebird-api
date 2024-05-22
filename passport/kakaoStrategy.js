33f15d51069090b3125feb7d61a4e769d65eeb86
KSG <tjdzms123@naver.com>
2024-05-22 14:43:26 +0900

    카카오 로그인 기능 구현

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -1,1 +1,37 @@
-module.exports = () => {};
+const passport = require("passport");
+const { Strategy: kakaoStrategy } = require("passport-kakao");
+const User = require("../models/user");
+
+module.exports = () => {
+  passport.use(
+    new kakaoStrategy(
+      {
+        clientID: process.env.KAKAO_ID,
+        callbackURL: "/auth/kakao/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log("profile", profile);
+        try {
+          const exUser = await User.findOne({
+            where: { snsId: profile.id, provider: "kakao" },
+          });
+          if (exUser) {
+            done(null, exUser);
+          } else {
+            const newUser = await User.create({
+              email: profile._json?.kakao_account?.email,
+              nick: profile.displayName,
+              snsId: profile.id,
+              provider: "kakao",
+            });
+
+            done(null, newUser);
+          }
+        } catch (error) {
+          console.error(error);
+          done(error);
+        }
+      }
+    )
+  );
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const passport = require("passport");
 const { isNotLoggedIn, isLoggedIn } = require("../middlewares");
-const { join, login, logout } = require('../controllers/auth');
+const { join, login, logout } = require("../controllers/auth");
 const router = express.Router();
 
-// POST /auth /join
+// POST /auth/join
 router.post("/join", isNotLoggedIn, join);
-// POST /auth /login
+// POST /auth/login
 router.post("/login", isNotLoggedIn, login);
-// GET /auth /logout
+// GET /auth/logout
 router.get("/logout", isLoggedIn, logout);
+
+// /auth/kakao
+router.get("/kakao", passport.authenticate("kakao")); // 카카오톡 로그인 화면으로 redirect
+
+// /auth/kakao -> 카카오 로그인 화면 -> /auth/kakao/callback
+
+// /auth/kakao/callback
+router.get(
+  "/kakao/callback",
+  passport.authenticate("kakao", {
+    failureRedirect: "/?loginError=카카오로그인 실패",
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
 
 module.exports = router;
